85df012c16febe1616fc99ed922605233385323f
krystlc <alex.valle@gmail.com>
2025-08-22 00:57:09 -0400

    restore

--- backend/codegen.ts
+++ backend/codegen.ts
@@ -1,9 +1,10 @@
 import type { CodegenConfig } from "npm:@graphql-codegen/cli";
+import { defineConfig } from "npm:@eddeee888/gcg-typescript-resolver-files";
 
 const config: CodegenConfig = {
   schema: "**/schema.graphql",
   generates: {
-    "graphql/schema": {},
+    "graphql/schema": defineConfig(),
   },
 };
 export default config;
