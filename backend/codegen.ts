505a0fafd8146b9d85e99fb8c178eb264136b872
krystlc <alex.valle@gmail.com>
2025-08-22 00:50:50 -0400

    fix: remove that gen thing

--- backend/codegen.ts
+++ backend/codegen.ts
@@ -1,10 +1,9 @@
 import type { CodegenConfig } from "npm:@graphql-codegen/cli";
-import { defineConfig } from "npm:@eddeee888/gcg-typescript-resolver-files";
 
 const config: CodegenConfig = {
   schema: "**/schema.graphql",
   generates: {
-    "graphql/schema": defineConfig(),
+    "graphql/schema": {},
   },
 };
 export default config;
